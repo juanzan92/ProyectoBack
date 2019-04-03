b229ca9b7119894adf0ae2e8bf2d26ea1b8946ce
Gabriel Neil <gabriel.neil@mercadolibre.com>
2019-04-03 19:51:23 -0300

    first commit

---
+++ src/main/java/tesis/controllers/PocController.java
@@ -0,0 +1,50 @@
+package tesis.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import tesis.dtos.entities.PocDTO;
+import tesis.services.impl.PocService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class PocController {
+    @Autowired
+    PocService pocService;
+
+    @GetMapping("/poc/string")
+    public String hola() {
+        return "has hecho una peticion get";
+    }
+
+    @GetMapping("/poc/map")
+    public Map<String, String> sayHello() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        map.put("aa", "bb");
+        return map;
+    }
+
+    @GetMapping("/poc/entity")
+    public PocDTO pocEntity() {
+        PocDTO poc = new PocDTO();
+        poc.setDescription("prueba_de_poc");
+        poc.setValue1(1);
+        return poc;
+    }
+
+    @GetMapping("/poc/service")
+    public PocDTO pocService() {
+        return pocService.getDTO(123);
+    }
+
+    @PostMapping("/poc/post")
+    public PocDTO addMemberV1(@RequestBody Object member) {
+        System.out.println("entró al post");
+        return pocService.getDTO(123);
+    }
+}
---
+++ src/main/java/tesis/services/interfaces/PocInterface.java
@@ -0,0 +1,9 @@
+package tesis.services.interfaces;
+
+import tesis.dtos.entities.PocDTO;
+
+public interface PocInterface {
+
+    PocDTO getDTO(int id);
+}
+
