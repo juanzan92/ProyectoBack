a0d086d8a714a31bc91bd451b6dbd961df4f3ef6
Gabriel Neil <gabriel.neil@mercadolibre.com>
2019-07-27 01:25:28 -0300

    clean up

--- src/main/java/tesis/controllers/mercadopago/MPController.java
+++ src/main/java/tesis/controllers/mercadopago/MPController.java
@@ -8,7 +8,6 @@ import com.mercadopago.resources.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tesis.entities.dtos.mercadopago.PreferenceDTO;
-import tesis.services.mercadopago.CardService;
 import tesis.services.mercadopago.PreferenceService;
 import tesis.services.mercadopago.UserServices;
 
@@ -21,34 +20,7 @@ public class MPController {
     @Autowired
     UserServices userServices;
     @Autowired
-    CardService cardService;
-    @Autowired
     PreferenceService preferenceService;
-
-    @PostMapping("/mp/users/create_user")
-    public String createUser() {
-        return userServices.createUser();
-    }
-
-    @GetMapping("/mp/users/get_user")
-    public Customer getUser() throws MPException {
-        return userServices.getUser("449730233-vpQkKAveTRQgJV");
-    }
-
-    @GetMapping("/mp/users/search_user")
-    public MPResourceArray searchUser() throws MPException {
-        return userServices.searchCustomer("todo_guion_bajo@yahoo.com");
-    }
-
-    @GetMapping("/mp/cards/get_user_cards")
-    public ArrayList<Card> getUserCards() throws MPException {
-        return cardService.getUserCards("449730233-vpQkKAveTRQgJV");
-    }
-
-    @PostMapping("/mp/cards/create_card")
-    public Card createCard() throws MPException {
-        return cardService.createCard("449730233-vpQkKAveTRQgJV");
-    }
 
     @PostMapping("/mp/users/marketplace_auth")
     public String createMarketplaceAuth(@RequestParam Map<String, String> allParams) throws JsonProcessingException {
--- src/main/java/tesis/services/mercadopago/UserServices.java
+++ src/main/java/tesis/services/mercadopago/UserServices.java
@@ -19,64 +19,9 @@ import java.util.HashMap;
 @Service
 public class UserServices {
     @Autowired
-    CardService cardService;
-    @Autowired
     RestClient restClient;
     @Autowired
     UserService userService;
-
-    public String createUser() {
-        try {
-            MercadoPago.SDK.configure("TEST-5912969040584293-092110-771d21ab7cc96c709fbcc464d05c409b-187271358");
-            Customer customer = createUser("todo_guion_bajo@yahoo.com");
-            cardService.createCard(customer);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    public Customer createUser(String email) {
-        Customer customer = new Customer();
-        try {
-            customer.setEmail(email);
-            customer.save();
-        } catch (MPException e) {
-            e.printStackTrace();
-        }
-
-        return customer;
-    }
-
-    public Customer getUser(String customerId) throws MPException {
-        MercadoPago.SDK.configure("TEST-5912969040584293-092110-771d21ab7cc96c709fbcc464d05c409b-187271358");
-
-        Customer customer = null;
-        try {
-            customer = Customer.findById(customerId);
-        } catch (MPException e) {
-            throw e;
-        }
-        return customer;
-    }
-
-    public MPResourceArray searchCustomer(String email) throws MPException {
-        MercadoPago.SDK.configure("TEST-5912969040584293-092110-771d21ab7cc96c709fbcc464d05c409b-187271358");
-        MPResourceArray customers = null;
-
-        try {
-            HashMap<String, String> filters = new HashMap<>();
-            filters.put("email", email);
-
-            customers = Customer.search(filters, false);
-
-        } catch (MPException e) {
-            throw e;
-        }
-        return customers;
-    }
 
     public String createMarketplaceAuth(String authCode, String username) throws JsonProcessingException {
         MultiValueMap<String, String> requestBody = VendorMarshaller.buildVendorForDynamo(authCode, username);
