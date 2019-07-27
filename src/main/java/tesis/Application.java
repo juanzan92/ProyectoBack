b82308260c1a52a943b335e6403b72e740d0ba32
Gabriel Neil <gabriel.neil@mercadolibre.com>
2019-07-27 15:38:39 -0300

    no message

--- src/main/java/tesis/Application.java
+++ src/main/java/tesis/Application.java
@@ -2,8 +2,6 @@ package tesis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
