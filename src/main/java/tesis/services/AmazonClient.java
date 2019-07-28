26d98c52eb541660c39e53ffea7b81717ef7db36
Gabriel Neil <gabriel.neil@mercadolibre.com>
2019-07-28 17:17:58 -0300

    removed unused imports

--- src/main/java/tesis/services/AmazonClient.java
+++ src/main/java/tesis/services/AmazonClient.java
@@ -5,13 +5,11 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
--- src/main/java/tesis/services/mercadopago/UserServices.java
+++ src/main/java/tesis/services/mercadopago/UserServices.java
@@ -1,20 +1,13 @@
 package tesis.services.mercadopago;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.mercadopago.MercadoPago;
-import com.mercadopago.core.MPResourceArray;
-import com.mercadopago.exceptions.MPException;
-import com.mercadopago.resources.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import tesis.entities.dtos.mercadopago.Vendor;
 import tesis.entities.marshallers.mercadopago.VendorMarshaller;
 import tesis.services.RestClient;
 import tesis.services.account.UserService;
-
-import java.util.HashMap;
 
 @Service
 public class UserServices {
