fc88ff385907df084639baec12fe0ff80ad1a497
Gabriel Neil <gabriel.neil@mercadolibre.com>
2019-07-29 15:20:56 -0300

    no message

--- src/main/java/tesis/entities/builders/dynamo/ItemBuilder.java
+++ src/main/java/tesis/entities/builders/dynamo/ItemBuilder.java
@@ -11,13 +11,16 @@ public class ItemBuilder {
     private static ForDynamo forDynamo = new ForDynamo("items", "itemId");
 
     public static Map buildDynamo(Item item) {
-
         HashMap<String, Map> dynamoItem = new HashMap<>();
-        dynamoItem.put("dynamo", new ObjectMapper().convertValue(forDynamo, Map.class));
+        queryParameteres(dynamoItem);
 
         Map<String, Object> map = new ObjectMapper().convertValue(item, Map.class);
         dynamoItem.put("object", map);
 
         return dynamoItem;
+    }
+
+    public static void queryParameteres(HashMap<String, Map> dynamoItem) {
+        dynamoItem.put("dynamo", new ObjectMapper().convertValue(forDynamo, Map.class));
     }
 }
