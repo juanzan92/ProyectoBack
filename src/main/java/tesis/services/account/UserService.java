bcca5cd8835a0b09b7e69f35473db3f745466129
yerbas <gervasio.posadas@gmail.com>
2019-08-12 14:48:30 -0300

    no message

--- src/main/java/tesis/services/account/UserService.java
+++ src/main/java/tesis/services/account/UserService.java
@@ -16,28 +16,30 @@ import java.util.Map;
 
 @Service
 public class UserService {
+
     @Autowired
     RestClient restClient;
-    //String urlBase = "https://lrxqme2z7k.execute-api.us-east-1.amazonaws.com/Prod/users";
+
     String urlBase = "https://rtge19cj13.execute-api.us-east-1.amazonaws.com/prod/generic_ep";
     ForDynamo forDynamo = new ForDynamo("users", "username");
 
     public String createUser(@NotNull User user) throws JsonProcessingException {
         return restClient.request(urlBase, DynamoBuilder.saveObject(user, forDynamo), HttpMethod.POST, String.class);
     }
-        // Los Consumer de MP los vamos a guardar en la tabla Users???? //
-    public Consumer getConsumer(@NotNull Map<String, String> param) throws JsonProcessingException {
-        return restClient.request(DynamoBuilder.getObject(param, forDynamo, urlBase), HttpMethod.GET, Consumer.class);
-    }
-        // Falta Actualizar , el create vendor es solo un update del user para pasarlo a Vendor????? porque el PUT sino? //
-        // Porque lleva 2 parametros de tipo persona la funcion? vendor y username????? //
+
+    // Falta Actualizar , el create vendor es solo un update del user para pasarlo a Vendor????? porque el PUT sino? //
+    // Porque lleva 2 parametros de tipo persona la funcion? vendor y username????? //
     public String createVendorUser(@NotNull Vendor vendor, @NotNull String username) throws JsonProcessingException {
         return restClient.request(urlBase, DynamoBuilder.saveObject(vendor, forDynamo), HttpMethod.PUT, String.class);
     }
-        // Los Vendors  de MP también los vamos a guardar en la tabla Users???? //
+
+    // Los Vendors  de MP también los vamos a guardar en la tabla Users???? //
     public Vendor getVendor(@NotNull Map<String, String> param) throws JsonProcessingException {
         return restClient.request(DynamoBuilder.getObject(param, forDynamo, urlBase), HttpMethod.GET, Vendor.class);
     }
 
-
+    // Los Consumer de MP los vamos a guardar en la tabla Users???? //
+    public Consumer getConsumer(@NotNull Map<String, String> param) throws JsonProcessingException {
+        return restClient.request(DynamoBuilder.getObject(param, forDynamo, urlBase), HttpMethod.GET, Consumer.class);
+    }
 }
