498984ee4a59807419221d03a9b9096ac090434f
yerbas <gervasio.posadas@gmail.com>
2019-09-02 21:56:25 -0300

    Added EP for PUT and DELETE for Home Controller (3x objects)

--- src/main/java/tesis/controllers/home/HomeController.java
+++ src/main/java/tesis/controllers/home/HomeController.java
@@ -10,6 +10,8 @@ import tesis.entities.dtos.home.ItemCarrouselMain;
 import tesis.entities.dtos.home.ItemHurryUp;
 import tesis.services.home.HomeService;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/catalog/items/home")
 public class HomeController {
@@ -19,31 +21,61 @@ public class HomeController {
 
     @PostMapping("/carrousel_main")
     public String createItemHomeCarrouselMain(@RequestBody ItemCarrouselMain itemCarrouselMain) throws JsonProcessingException {
-        return homeService.createItemHomeCarrouselMain(itemCarrouselMain);
+        return homeService.createItemCarrouselMain(itemCarrouselMain);
     }
 
     @GetMapping("/carrousel_main/search")
     public ItemCarrouselMain[] searchItemsHomeCarrouselMain() throws JsonProcessingException {
-        return homeService.searchItemsHomeCarrouselMain();
+        return homeService.searchItemsCarrouselMain();
+    }
+
+    @PutMapping("/carrousel_main")
+    public String updateItemCarrouselMain(@RequestBody ItemCarrouselMain itemCarrouselMain) throws JsonProcessingException {
+        return homeService.updateItemCarrouselMain(itemCarrouselMain);
+    }
+
+    @DeleteMapping("/carrousel_main")
+    public String deleteItemCarrouselMain(@RequestBody Map<String, String> param) throws JsonProcessingException {
+        return homeService.deleteItemCarrouselMain(param);
     }
 
     @PostMapping("/hurry_up")
-    public String createItemHomeHurryUp(@RequestBody ItemHurryUp itemHurryUp) throws JsonProcessingException {
-        return homeService.createItemHomeHurryUp(itemHurryUp);
+    public String createItemHurryUp(@RequestBody ItemHurryUp itemHurryUp) throws JsonProcessingException {
+        return homeService.createItemHurryUp(itemHurryUp);
     }
 
     @GetMapping("/hurry_up/search")
     public ItemHurryUp[] searchItemsHomeHurryUp() throws JsonProcessingException {
-        return homeService.searchItemsHomeHurryUp();
+        return homeService.searchItemsHurryUp();
+    }
+
+    @PutMapping("/hurry_up")
+    public String updateItemHurryUp(@RequestBody ItemHurryUp itemHurryUp) throws JsonProcessingException {
+        return homeService.updateItemHurryUp(itemHurryUp);
+    }
+
+    @DeleteMapping("/hurry_up")
+    public String deleteItemHurryUp(@RequestBody Map<String, String> param) throws JsonProcessingException {
+        return homeService.deleteItemHurryUp(param);
     }
 
     @PostMapping("/carrousel_bottom")
     public String createItemHomeCarrouselBottom(@RequestBody ItemCarrouselBottom itemCarrouselBottom) throws JsonProcessingException {
-        return homeService.createItemHomeCarrouselBottom(itemCarrouselBottom);
+        return homeService.createItemCarrouselBottom(itemCarrouselBottom);
     }
 
     @GetMapping("/carrousel_bottom/search")
     public ItemCarrouselBottom[] searchItemsHomeCarrouselBottom() throws JsonProcessingException {
-        return homeService.searchItemsHomeCarrouselBottom();
+        return homeService.searchItemsCarrouselBottom();
+    }
+
+    @PutMapping("/carrousel_bottom")
+    public String updateItemCarrouselBottom(@RequestBody ItemCarrouselBottom itemCarrouselBottom) throws JsonProcessingException {
+        return homeService.updateItemCarrouselBottom(itemCarrouselBottom);
+    }
+
+    @DeleteMapping("/carrousel_bottom")
+    public String deleteItemCarrouselBottom(@RequestBody Map<String, String> param) throws JsonProcessingException {
+        return homeService.deleteItemCarrouselBottom(param);
     }
 }
