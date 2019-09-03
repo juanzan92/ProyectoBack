76a737b2a0348d99c1c65c350f1bfbc92a4cd10f
yerbas <gervasio.posadas@gmail.com>
2019-09-03 11:41:19 -0300

    Updated Item Controller : changed REQUESTBODY for REQUESTPARAM @ GET method

--- src/main/java/tesis/controllers/item/ItemController.java
+++ src/main/java/tesis/controllers/item/ItemController.java
@@ -23,8 +23,9 @@ public class ItemController {
     }
 
     @GetMapping()
-    public Item getItem(@RequestBody Map<String, String> param) throws JsonProcessingException {
-        return itemService.getItem(param);
+    // public Item getItem(@RequestBody Map<String, String> param) throws JsonProcessingException {
+    public Item getItem(@RequestParam Map<String, String>  item_id) throws JsonProcessingException {
+        return itemService.getItem(item_id);
     }
 
     @GetMapping("/get_all")
