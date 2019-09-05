92a439dd100429f1e1cbcb2aa3cb777d55d4197a
yerbas <gervasio.posadas@gmail.com>
2019-09-04 22:28:46 -0300

    no message

---
+++ src/main/java/tesis/controllers/item/ReviewController.java
@@ -0,0 +1,10 @@
+package tesis.controllers.item;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/catalog/reviews")
+public class ReviewController {
+
+}
