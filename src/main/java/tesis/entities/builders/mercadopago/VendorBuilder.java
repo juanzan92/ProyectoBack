caf2435d9594b1f39010e231c0468af56a09251c
Gabriel Neil <gabriel.neil@mercadolibre.com>
2019-11-05 14:02:29 -0300

    client id de app owner

--- src/main/java/tesis/entities/builders/mercadopago/VendorBuilder.java
+++ src/main/java/tesis/entities/builders/mercadopago/VendorBuilder.java
@@ -9,8 +9,8 @@ public class VendorBuilder {
 
     public static MultiValueMap<String, String> buildVendorForDynamo(@NotNull String authCode, @NotNull String userId) {
         MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
-        requestBody.add("client_id", "7662807553309957");
-        requestBody.add("client_secret", "bhvJ3QKMypEzzphnnLhqdkQ5LVVZQx1n");
+        requestBody.add("client_id", "6593089027483502");
+        requestBody.add("client_secret", "DBYDXXgWZVdfXO1437liddRkn1LpJfTJ");
         requestBody.add("grant_type", "authorization_code");
         requestBody.add("code", authCode);
         requestBody.add("redirect_uri", "http://localhost:3000/splash?user_id=" + userId);
